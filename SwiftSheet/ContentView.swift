3fd0ef381125e7b514e314b57a862090ba38a1ba
adnanaddy <adnankhan545@gmail.com>
2025-05-22 00:13:04 +0500

    Initial Commit

---
+++ SwiftSheet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftSheet
+//
+//  Created by Adnan Khan AdDY on 5/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftSheet/SwiftSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftSheetApp.swift
+//  SwiftSheet
+//
+//  Created by Adnan Khan AdDY on 5/22/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
